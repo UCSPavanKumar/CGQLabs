1e35f488bce01c374dae945d5fb2a3d99710dd2e
UCSPavanKumar <pavanupadrasta@gmail.com>
2025-07-23 17:55:12 +0530

    removed send sm1xs

--- students/urls.py
+++ students/urls.py
@@ -2,6 +2,6 @@ from django.urls import path,include
 from . import views
 urlpatterns = [
     path('', views.index, name='index'),
-    path('sendsms/<str:content>/<str:phone_number>/', views.sendsms, name='sendsms'),
+    #path('sendsms/<str:content>/<str:phone_number>/', views.sendsms, name='sendsms'),
 ]
 
--- students/views.py
+++ students/views.py
@@ -8,17 +8,3 @@ import os
 def index(request):
     return render(request, 'students/index.html')
 
-def sendsms(request,content,phone_number):
-    """
-    Send text sms using twilio SMS API
-    """
-    account_sid = os.getenv('account_sid')
-    auth_token = os.getenv('auth_token')
-    print(account_sid, auth_token)
-    client = Client(account_sid, auth_token)
-    message = client.messages.create(
-    from_='+18482799449',
-    body=content,
-    to=phone_number
-    )
-    return HttpResponse(f"Message sent to {phone_number}: {message.sid}")
